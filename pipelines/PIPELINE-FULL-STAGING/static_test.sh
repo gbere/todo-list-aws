ccc2a732e985e5cd2000ebcf21bbbe10998d1e37
Gerard Berengué <gerard@bere.cat>
2023-01-28 10:21:35 +0000

    Enhance rad errors messages

--- pipelines/PIPELINE-FULL-STAGING/static_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/static_test.sh
@@ -7,13 +7,15 @@ RAD_ERRORS=$(radon cc src -nc | wc -l)
 
 if [[ $RAD_ERRORS -ne 0 ]]
 then
-    echo 'Ha fallado el análisis estatico de RADON - CC'
+    echo 'Ha fallado el análisis estatico de RADON. La complegidad ciclomática ' \ 
+         '(flag cc), se esperaba que fuese calidad B o superior' \ 
+         '(flag -nc lista errores de calidad C o peores)'
     exit 1
 fi
 RAD_ERRORS=$(radon mi src -nc | wc -l)
 if [[ $RAD_ERRORS -ne 0 ]]
 then
-    echo 'Ha fallado el análisis estatico de RADON - MI'
+    echo 'Ha fallado el análisis estatico de RADON en el indice de mantenibilidad (flag mi)'
     exit 1
 fi
 
